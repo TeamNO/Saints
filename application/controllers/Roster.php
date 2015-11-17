87310ef192eaafa7496de362b31f176847e0ac50
Ignacio Marquez <igsmar@gmail.com>
2015-11-16 18:01:05 -0800

    pagination for roster works properly now

--- application/controllers/Roster.php
+++ application/controllers/Roster.php
@@ -8,10 +8,28 @@ class Roster extends Application
      */
     public function index()
     {
-        $this->data['pagebody'] = 'rosterview';    // this is the view we want shown
-        $roster = array();
-        $roster = $this->playerroster->all();
+        $this->data['pagebody'] = 'rosterview';    // this is the view we want show
+
+        // setting up pagination
+        $config = array();
+        $config['base_url'] = '/roster/index';
+        $config['total_rows'] = $this->playerroster->record_count();
+        $config['per_page'] = 12;
+        $config['uri_segment'] = 3;
+
+        $this->pagination->initialize($config);
+
+        $page = ($this->uri->segment(3)) ? $this->uri->segment(3) : 0;
+
+        //$data['saintroster'] = $this->playerroster->fetch_players($config['per_page'], $page);
+        $roster = $this->playerroster->fetch_players($config['per_page'], $page);
+        echo $this->pagination->create_links();
         $this->data['saintroster'] = $roster;
-        $this->render();	
+        $this->render();
+
+        //$roster = array();
+        //$roster = $this->playerroster->all();
+        //$this->data['saintroster'] = $roster;
+        //$this->render();	
     }
 }
--- application/models/PlayerRoster.php
+++ application/models/PlayerRoster.php
@@ -11,7 +11,7 @@
  *
  * @author a00860087
  */
-class PlayerRoster extends My_Model{
+class PlayerRoster extends MY_Model{
     //put your code here
      function __construct() {
         parent::__construct('roster', 'id');
@@ -21,5 +21,24 @@ class PlayerRoster extends My_Model{
         $CI = &get_instance();
         $query = $CI->db->get('roster');
         return $query->result_array();
+    }
+
+    function record_count() {
+    	$CI = &get_instance();
+    	return $CI->db->count_all('roster');
+    }
+
+    function fetch_players($limit, $start) {
+    	$CI = &get_instance();
+    	$CI->db->limit($limit, $start);
+    	$query = $CI->db->get('roster');
+
+    	if($query->num_rows()>0) {
+    		foreach($query->result() as $row) {
+    			$data[] = $row;
+    		}
+    		return $data;
+    	}
+    	return false;
     }
 }
