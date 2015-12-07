8a281c00bf34ecb40c9723ced1365821c8485c8b
sschocolate <poshenhuang123@hotmail.com>
2015-12-06 16:29:26 -0800

    added xml data retrieval from Jim's server. Created function that parses xml data but does not update the db yet.

--- application/models/Standing.php
+++ application/models/Standing.php
@@ -15,8 +15,38 @@ class Standing extends My_Model{
     //put your code here
         function __construct() {
         parent::__construct('standing', 'id');
+        $this->load->library('xmlrpc');
+        $this->xmlrpc->server("nfl.jlparry.com/rpc", 80);
+	$this->xmlrpc->method('since');
+        $request = array('19321203');
+        $this->xmlrpc->request($request);
+        if ( ! $this->xmlrpc->send_request()) {
+            echo $this->xmlrpc->display_error();
+        } else {	
+            $this->xmlResult = $this->xmlrpc->display_response();
+        }
+    }
+    
+    function updateHistory(){
+        $scores = array();
+        foreach($this->xmlResult as $data) {
+                $data = $this->mapData($data);
+                array_push($scores, $data);
+        }
     }
 
+    function mapData($source){
+        $record = new stdClass();
+        //parse home and away team scores by tokenizing the ':' delimiter of xml result
+        $scores = explode(":", $source["score"]);
+        $record->home = $source['home'];
+        $record->date = (new DateTime($source["date"]))->format('Y-m-d');
+        $record->homeScore = $scores[0]; //parsed home score
+        $record->awayScore = $scores[1]; //parsed away score
+        $record->away = $source['away'];
+        return $record;
+    }
+    
     function all() {
         $CI = &get_instance();
         $query = $CI->db->get('standing');
